377cc8e1ccb65012d3a46925b0396554060b391d
Andy Chang <andymjchang@gmail.com>
2025-07-06 22:56:07 -0600

    Changed gamemodebase to reflect new file structure
    [git-p4: depot-paths = "//frog/main/project/": change = 43]

--- Source/Frog/FrogGameMode.cpp
+++ Source/Frog/FrogGameMode.cpp
@@ -7,7 +7,7 @@
 AFrogGameMode::AFrogGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_FrogCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/BP_FrogCharacter"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
