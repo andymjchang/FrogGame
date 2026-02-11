156e6aa936a1e62a11262411e4f4837095e1a586
Andy Chang <andymjchang@gmail.com>
2026-02-11 14:15:22 -0600

    Add interactable class to VCS
    [git-p4: depot-paths = "//frog/main/project/": change = 313]

---
+++ Source/Frog/FrogGameplay/Interactable.cpp
@@ -0,0 +1,57 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interactable.h"
+
+#include "Components/BoxComponent.h"
+
+AInteractable::AInteractable()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	bReplicates = true;
+	
+	// Root Component
+	RootSceneComponent = CreateDefaultSubobject<USceneComponent>(TEXT("RootSceneComponent"));
+	SetRootComponent(RootSceneComponent);
+	
+	// Interact Hitbox
+	InteractHitBox = CreateDefaultSubobject<UBoxComponent>(TEXT("InteractHitBox"));
+	InteractHitBox->SetupAttachment(RootComponent);
+	InteractHitBox->SetCollisionProfileName(TEXT("InteractListen"));
+	InteractHitBox->InitBoxExtent(FVector(128.f, 128.f, 128.f));
+	InteractHitBox->SetRelativeLocation(FVector(0.0f, 0.0f, 128.0f));
+	
+	// Static Mesh Component
+	InteractableMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("InteractableMesh"));
+	InteractableMesh->SetupAttachment(RootComponent);
+}
+
+void AInteractable::Interact()
+{
+}
+
+void AInteractable::StartHighlight()
+{
+	if (IsValid(InteractableMesh) && IsValid(OverlayMaterial))
+	{
+		InteractableMesh->SetOverlayMaterial(OverlayMaterial);
+	}
+}
+
+void AInteractable::StopHighlight()
+{
+	if (IsValid(InteractableMesh))
+	{
+		InteractableMesh->SetOverlayMaterial(nullptr);
+	}
+}
+
+void AInteractable::EnableInteractable()
+{
+	if (IsValid(InteractHitBox)) InteractHitBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+}
+
+void AInteractable::DisableInteractable()
+{
+	if (IsValid(InteractHitBox)) InteractHitBox->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
---
+++ Source/Frog/FrogGameplay/Interactable.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Interactable.generated.h"
+
+class UItemData;
+class UBoxComponent;
+
+UCLASS()
+class FROG_API AInteractable : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AInteractable();
+	
+	virtual void Interact();
+
+	void StartHighlight();
+	void StopHighlight();
+	
+	void EnableInteractable();
+	void DisableInteractable();
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "User Settings")
+	TObjectPtr<UItemData> Data;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "User Settings")
+	TObjectPtr<UMaterialInterface> OverlayMaterial;
+	
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<USceneComponent> RootSceneComponent;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UBoxComponent> InteractHitBox;
+	
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UStaticMeshComponent> InteractableMesh;
+
+public:
+	FORCEINLINE UItemData* GetData() const { return Data; }
+};
