d32515257959b3be7029b4b402070c034bb0ad00
Andy Chang <andymjchang@gmail.com>
2026-05-28 01:17:15 -0500

    Added header file to comments

--- Source/Frog/FrogGameplay/Core/ContainerComponent.cpp
+++ Source/Frog/FrogGameplay/Core/ContainerComponent.cpp
@@ -41,6 +41,72 @@ void IItemInterface::EventAddedToAnotherInventory()
 	DisableHitbox();
 }
 
+////////////////
+Header file, if you don't want to change files to look at it on GitHub
+////////////////
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInventoryItemChanged, const TScriptInterface<IItemInterface>&, Item);
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class FROG_API UContainerComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+	UContainerComponent();
+	void Initialize(UItemData* InData, UInteractableWidgetComponent* InWidgetComponent, UMeshComponent* InMesh);
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+	void ClearInventory();
+	void RemoveNullsFromInventory();
+	bool TryAddToInventory(const TScriptInterface<IItemInterface>& InteractableToAdd, UContainerComponent* SourceContainerComp = nullptr, bool
+	                       bRemoveFromSource = true);
+	bool TryAddContainerContentsToInventory(UContainerComponent* SourceContainerComp);
+	bool TryRemoveFromInventory(TScriptInterface<IItemInterface> InteractableToRemove);
+
+	void SetShowInventoryWidget(bool bShow);
+
+	// Delegates
+	FOnInventoryItemChanged OnRemovedFromInventory;
+	FOnInventoryItemChanged OnAddedToInventory;
+
+	bool IsEmpty() const { return Inventory.Num() <= 0; }
+	bool IsFull() const { return Data.IsValid() ? Inventory.Num() >= Data->GetMaxCapacity() : true; }
+	TScriptInterface<IItemInterface> GetFirstItem() const { return Inventory.Num() > 0 ? Inventory[0] : nullptr; }
+	TArray<TScriptInterface<IItemInterface>> GetInventory() const { return Inventory; }
+	int GetInventorySize() const { return Inventory.Num(); }
+	bool GetAllowAdd() const { return bAllowAdd; }
+	void SetAllowAdd(const bool InAllowAdd) { bAllowAdd = InAllowAdd; }
+	bool GetAllowRemove() const { return bAllowRemove; }
+	void SetAllowRemove(const bool InAllowRemove) { bAllowRemove = InAllowRemove; }
+	
+protected:
+	void UpdateInventoryWidget();
+
+	UFUNCTION()
+	void OnRep_Inventory();
+
+	UFUNCTION()
+	void OnRep_IsInventoryWidgetVisible();
+
+protected:
+	UPROPERTY(ReplicatedUsing = OnRep_IsInventoryWidgetVisible)
+	bool bIsInventoryWidgetVisible = true;
+	
+	UPROPERTY(Transient, Replicated)
+	bool bAllowAdd = true;
+	
+	UPROPERTY(Replicated)
+	bool bAllowRemove = true;
+
+	UPROPERTY(ReplicatedUsing = OnRep_Inventory)
+	TArray<TScriptInterface<IItemInterface>> Inventory;
+	
+	TWeakObjectPtr<UInteractableWidgetComponent> InventoryWidgetComponent;
+	TWeakObjectPtr<UItemData> Data;
+	TWeakObjectPtr<UMeshComponent> Mesh;
+};
+
  */
 #include "ContainerComponent.h"
 
